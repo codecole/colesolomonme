50debc11e116d847b42540e55d61f60ce8649176
github.io/codecole <cole.iceneil@gmail.com>
2020-08-30 17:42:36 +0100

    first version

---
+++ design-detail/ui-case-detail.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="theme-color" content="#282828" />
+    <title>Cole Solomon | Ux Engineer: Interaction design & App Development</title>
+    <meta name="author" content="Cole Solomon Ux Engineer">
+    <meta name="description" content="Cole Solomon | Ux Engineer: Interaction design & App Development">
+    <meta name="keywords" content="creative, works, showcase, portfolio, highlight, projects, modern, agency, digital, studio, css, animation, transition, freelancers,app, interaction, user experience, ux, ui, research, engineering, design, dribble, behance, developer">
+
+    <!-- SOCIAL MEDIA META -->
+    <meta property="og:description" content="Cole Solomon | Ux Engineer: Interaction design & App Development">
+    <meta property="og:image" content="https://www.colesolomon.me">
+    <meta property="og:site_name" content="colesolomonme">
+    <meta property="og:title" content="colesolomonme">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.colesolomon.me">
+
+    <!-- TWITTER META -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@thecodeghost">
+    <meta name="twitter:creator" content="@thecodeghost">
+    <meta name="twitter:title" content="thecodeghost">
+    <meta name="twitter:description" content="Cole Solomon | Ux Engineer: Interaction design & App Development">
+    <meta name="twitter:image" content="https://www.colesolomon.me">
+
+    <!-- FAVICON FILES -->
+    <link href="/ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
+    <link href="/ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="/ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="/ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
+    <link href="/ico/favicon.png" rel="shortcut icon">
+
+    <!-- CSS FILES -->
+    <link rel="stylesheet" href="../css/w3.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+    <link rel="stylesheet" href="../css/odometer.min.css">
+    <link rel="stylesheet" href="../css/fancybox.min.css">
+    <link rel="stylesheet" href="../css/swiper.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+ </head>
+
+<body>
+
+
+    <?php  include "includes/headerNav2.html" ;?>
+
+    <section class="page-header">
+        <figure>
+            <img src="../images/ui-design2-featured.png" alt="social_elearning_ui_design"></a>
+        </figure>
+    </section>
+
+    <section class="case-detail">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <h6>CASE TIMELINE</h6>
+                    <p>5 Weeks</p>
+                </div>
+                <!-- end col-6 -->
+                <div class="col-md-4">
+                    <h6>ROLE</h6>
+                    <p>UX Designer - Research |Visual Design|Information Architecture|Style Guide| Interaction</p>
+                </div>
+                <div class="col-md-4">
+                    <h6>TOOLS</h6>
+                    <p>Figma | Adobe Illustrator | Balsamiq | Adobe Photoshop </p>
+
+                </div>
+                <div class="col-lg-12">
+                    <h5>CASE REVIEW</h5>
+                    <p>Genius TEchnology wanted to redesign an existing mobile app; Qme, this was suitable due to how their solutions was adopted and been that their major audience, were students and young professionals. Qme is a social eLearning platform,
+                        that enables users to connect via the forum, share materials and prepare for upcoming examinations as well as stay updated to the latest trends around their interest. </p>
+                </div>
+                <div>
+                    <h5>CHALLENGE</h5>
+                    <p>The goal of Genius Technology was to improve the experience and interface of Qme and to be able to feat the new features, they were integrating into the app.</p>
+                    <p>The previous design as seen below, I ensured that I outline the essential features that already existed on their solution, researched on the missing features that would aid a an amiable experience, yet not deviate totally from the
+                        existing branding
+                    </p>
+                </div>
+
+
+                <!-- end col-8 -->
+
+                <!-- end col-6 -->
+                <div class="col-12">
+                    <figure>
+                        <img src="../images/ui-design2-detail02.png" alt="social_elearning_ui_redesign_former"></a>
+                    </figure>
+                </div>
+
+                <!-- end col-12 -->
+                <div class="col-lg-12">
+                    <h5>PROCESS</h5>
+
+                    <ol>
+                        <li>Discover - Usability review | Research on similar apps</li>
+                        <li>Understand - Document findings </li>
+                        <li> Ideate - Mid Fidelity | High Fidelity </li>
+                        <li>Review - User testing</li>
+                    </ol>
+                    </p>
+                    <h5>RESEARCH</h5>
+                    <p>The stakeholders sent a the previous design, along with new features, they needed to implement and I had to study it intensively to better understand the goal of the social eLearning Tool. This helped me to better understand the features
+                        associated with this tool and why those features existed. Next, I interviewed existing users, on what were the hookups they experienced using the app, what they really enjoyed most out of the app, the features they interact with
+                        frequently and what features they would like to see. I also explored other similar applications to understand how they work and to ascertain how best to visually represent features on the mobile app. This helped me grasp the current
+                        experience, improve the direction for the user flow.
+                    </p>
+
+                    <h5>FEATURES</h5>
+                    <p>The existing app lacked an onboarding process, as this helped existing and new users to familiarise themselves with the existing and new features of the applicaton. The onboarding included, well crafted illustrations to create a fun,
+                        friendly and calm experience, to aid the slogan " learning with fun"
+
+                    </p>
+                    <p>
+                        I had to also improve the authentication process which earlier used mobile number for authentication , the interface was not meeting modern standard and some of the hookups and issues raised by some users was difficulty in signing up and getting verified.
+                        Most got through to the app after numeorus attempt to sign up as users. I redesigned the interface, as well as the experience which took less than 2 minutes for users to get to the main feed.
+                    </p>
+
+                </div>
+                <div class="col-12">
+                    <figure><img src="../images/ui-design2-detail03.png" alt="social_elearning_ui_design_features"></figure>
+                </div>
+                <div class="col-lg-12">
+                    <h5>FEATURES</h5>
+                    <p>The main feed, that existed was just generated irrespective of the user's interest or goal of using the app and the NEWS FEATURE, this feature was paramount to users , as well as to the stakeholders as this was expected to make it
+                        a habit for users to use their product. I designed a section from the onboarding, that enabled users select areas of interest and which can always be updated from the profile, this helped deliver content that each user was interested
+                        in and readily want to return to not because they have to.about-intro </p>
+
+                    <p>Another new, feature QMe was to be improved with was integration of adverts into the application, as a lot of request were made by popular Educational Insitutes, that conduct A-level programs as well as other partners I had to redesign
+                        the home feed and added a section that displayed ads whoch was based on their interest selected, without interrupting or causing irritation to the users
+
+                    </p>
+                </div>
+
+                <div class="col-12">
+                    <figure><img src="../images/ui-design2-detail04.png" alt="social_elearning_ui_design_features"></figure>
+                </div>
+                <div class="col-lg-12">
+                    <h5>GENERAL IMPROVEMENTS</h5>
+                    <p> There was a general redesign of the solution's intrface, and this a summary of the features that were greatly affected:
+                        <ul>
+                            <li>Onboarding - Introduced this feature</li>
+                            <li>Authentication-Improved the authentication time and flow</li>
+                            <li> News feed - Delivered preferred content to users based of the (select interest section) </li>
+                            <li>Rating & Reviews - Users can have an overview on how others feel about each material, they wanted before downloading or purchase, as well as give feedbacks </li>
+                            <li>Library - An easy switch between materials that had been treated or not, coupled with a measure on progress, update on an existing material</li>
+                            <li>Home page- Implemented the advert section, without having to irritate users</li>
+
+                        </ul>
+                        The style guide was updated also, with a new set of colors, that didn't deviate from the current brand but still made it vibrant with a new saturation of orange used, as well as a new typeface which was better legible and appealed learning
+                    </p>
+                </div>
+                <!-- end col-8 -->
+                <div class="col-lg-6">
+                    <figure><img src="../images/ui-design2-detail05.png" alt="social_elearning_ui_design_color_palette"></figure>
+                </div>
+                <!-- end col-6 -->
+                <div class="col-lg-6">
+                    <figure><img src="../images/ui-design2-detail06.png" alt="social_elearning_ui_design_typography"></figure>
+                </div>
+                <!-- end col-6 -->
+                <div class="col-lg-12">
+                    <h6>TESTING</h6>
+                    <p>I was so excited to test this new design, the stakeholders tested the prototype and really saw a great improvement with the user interface, then we went further to test with some existing users, From the test session, I noted that
+                        there was better navigation experience for the users, the onboarding really helped alot of users see some functionalities they had not found out existed from the app, such as the offline feature and this was a major concern as
+                        previously, users felt that internet was a requirement to the materials, they had viewed. Some issues were raised such as having to browse through the materials in the library section , while users wanted to be able to do that
+                        from the home screen , once they dive into the app. I added a floating button, that helped users search easily as it was a major concern After changes were made, another test session was conducted for existing users, feedback from
+                        these sessions were key to iterating the interface design into the final solution.</p>
+                </div>
+                <!-- end col-6 -->
+                <!-- <div class="col-md-6">
+                <h6>ICONS</h6>
+                <p>Iconify Flat Icon Set</p>
+            </div> -->
+                <!-- end col-6 -->
+                <div class="col-12">
+                    <figure><img src="../images/ui-design2-detail07.png" alt="social_elearning_ui_design_mockup"></figure>
+                </div>
+                <!-- end col-12 -->
+                <div class="col-12">
+                    <h3>Thanks for Watching</h3>
+                </div>
+                <!-- end col-12 -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </section>
+
+    <div class="shuffle-link"> <span></span><a href="../design.php">RETURN TO PIXELS</a> </div>
+
+
+
+    <?php include "includes/footer2.html";?>
+
+    <!-- end footer -->
+    <audio id="hamburger-hover" src="../audio/link.mp3" preload="auto"></audio>
+
+    <!-- JS FILES -->
+
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/isotope.min.js"></script>
+    <script src="../js/swiper.min.js"></script>
+    <script src='../js/TweenMax.min.js'></script>
+    <script src='../js/odometer.min.js'></script>
+    <script src='../js/fancybox.min.js'></script>
+    <script src='../js/wow.min.js'></script>
+    <script src='../js/scripts.js'></script>
+
+
+
+</body>
+
+</html>
---
+++ index.js
@@ -0,0 +1,68 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//adds the text blur the left
+function addEffect() {
+    let effectMe2 = document.getElementById('landing');
+    effectMe2.classList.add("lefts");
+
+    let effectMe = document.getElementById('leftPane');
+    effectMe.classList.add("text-blur-out");
+
+
+
+    let effectMe1 = document.getElementById('ll');
+    effectMe1.classList.remove("w3-hide")
+
+}
+
+//removes text blur
+function removeEffect() {
+    let effectMe2 = document.getElementById('landing');
+    effectMe2.classList.remove("lefts");
+    let effectMe = document.getElementById('leftPane');
+    effectMe.classList.remove("text-blur-out");
+
+    let effectMe1 = document.getElementById('ll');
+    effectMe1.classList.add("w3-hide");
+
+}
+
+//adds text blut to the right
+function addnewEffect() {
+    let effectMe2 = document.getElementById('landing');
+    effectMe2.classList.add("rights");
+
+    let effectMe = document.getElementById('rightPane');
+    effectMe.classList.add("text-blur-out");
+
+    let effectMe1 = document.getElementById('rr');
+    effectMe1.classList.remove("w3-hide")
+}
+
+//removes text blur to the right
+function removenewEffect() {
+    let effectMe2 = document.getElementById('landing');
+    effectMe2.classList.remove("rights");
+    let effectMe = document.getElementById('rightPane');
+    effectMe.classList.remove("text-blur-out");
+
+    let effectMe1 = document.getElementById('rr');
+    effectMe1.classList.add("w3-hide");
+
+
+}
+
+//adds the exit function when clicked
+// function exitRight() {
+//     let exitRight = document.getElementsById('pane');
+//     exitRight.classList.add("slide-out-right");
+
+// }
